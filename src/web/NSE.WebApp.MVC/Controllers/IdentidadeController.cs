20001ee3e14a34aa96666acf80e355bcd17b0345
PauloAlves8039 <pj38alves@gmail.com>
2022-09-11 21:11:44 -0300

    Consumindo uma API via HttpService

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        private readonly IAutenticacaoService _autenticacaoService;
+
+        public IdentidadeController(IAutenticacaoService autenticacaoService)
+        {
+            _autenticacaoService = autenticacaoService;
+        }
+
         [HttpGet]
         [Route("nova-conta")]
         public IActionResult Registro()
@@ -17,7 +25,12 @@ namespace NSE.WebApp.MVC.Controllers
         [Route("nova-conta")]
         public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro) 
         {
-            if (!ModelState.IsValid) return View(usuarioRegistro);
+            if (!ModelState.IsValid) 
+            { 
+                return View(usuarioRegistro);
+            }
+
+            var resposta = await _autenticacaoService.Registro(usuarioRegistro);
 
             if (false) 
             {
@@ -39,6 +52,8 @@ namespace NSE.WebApp.MVC.Controllers
         public async Task<IActionResult> Login(UsuarioLogin usuarioLogin) 
         {
             if (!ModelState.IsValid) return View(usuarioLogin);
+
+            var resposta = await _autenticacaoService.Login(usuarioLogin);
 
             if (false)
             {
