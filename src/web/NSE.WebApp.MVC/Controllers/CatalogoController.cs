f2d648427add39f618634b16183736be49149d39
PauloAlves8039 <pj38alves@gmail.com>
2022-09-26 20:53:12 -0300

    Criando elementos para o Catalogo

---
+++ src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class CatalogoController : MainController
+    {
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("produto-detalhe/{id}")]
+        public async Task<IActionResult> ProdutoDetalhe(Guid id)
+        {
+            return View();
+        }
+    }
+}
