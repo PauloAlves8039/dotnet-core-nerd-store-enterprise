afd1d67486b6d51c3c680275adceb1f17c5509b7
PauloAlves8039 <pj38alves@gmail.com>
2022-10-21 17:50:32 -0300

    Implementando métodos na classe MessageBus

--- src/building blocks/NSE.MessageBus/MessageBus.cs
+++ src/building blocks/NSE.MessageBus/MessageBus.cs
@@ -1,8 +1,8 @@
 ﻿using EasyNetQ;
 using NSE.Core.Messages.Integration;
+using Polly;
+using RabbitMQ.Client.Exceptions;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NSE.MessageBus
@@ -16,62 +16,83 @@ namespace NSE.MessageBus
         public MessageBus(string connectionString)
         {
             _connectionString = connectionString;
+            TryConnect();
         }
 
-        public bool IsConnected => throw new NotImplementedException();
+        public bool IsConnected => _bus?.IsConnected ?? false;
 
 
         public void Publish<T>(T message) where T : IntegrationEvent
         {
-            throw new NotImplementedException();
+            TryConnect();
+            _bus.Publish(message);
         }
 
-        public Task PublishAsync<T>(T message) where T : IntegrationEvent
+        public async Task PublishAsync<T>(T message) where T : IntegrationEvent
         {
-            throw new NotImplementedException();
+            TryConnect();
+            await _bus.PublishAsync(message);
         }
 
         public TResponse Request<TRequest, TResponse>(TRequest request)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage
         {
-            throw new NotImplementedException();
+            TryConnect();
+            return _bus.Request<TRequest, TResponse>(request);
         }
 
-        public Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+        public async Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage
         {
-            throw new NotImplementedException();
+            TryConnect();
+            return await _bus.RequestAsync<TRequest, TResponse>(request);
         }
 
         public IDisposable Respond<TRequest, TResponse>(Func<TRequest, TResponse> responder)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage
         {
-            throw new NotImplementedException();
+            TryConnect();
+            return _bus.Respond(responder);
         }
 
         public IDisposable RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage
         {
-            throw new NotImplementedException();
+            TryConnect();
+            return _bus.RespondAsync(responder);
         }
 
         public void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class
         {
-            throw new NotImplementedException();
+            TryConnect();
+            _bus.Subscribe(subscriptionId, onMessage);
         }
 
         public void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class
         {
-            throw new NotImplementedException();
+            TryConnect();
+            _bus.SubscribeAsync(subscriptionId, onMessage);
+        }
+
+        private void TryConnect() 
+        {
+            if (IsConnected) return;
+
+            var policy = Policy.Handle<EasyNetQException>()
+                .Or<BrokerUnreachableException>()
+                .WaitAndRetry(3, retryAttempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+            policy.Execute(() => { _bus = RabbitHutch.CreateBus(_connectionString); });
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _bus.Dispose();
         }
     }
 }
