bdda3e3253161ac34d864bd3028bea155e4b6c0a
PauloAlves8039 <pj38alves@gmail.com>
2022-10-23 13:53:11 -0300

    Completando a customizaçao do EventBus

--- src/services/NSE.Costumer.API/Services/RegistroClienteIntegrationHandler.cs
+++ src/services/NSE.Costumer.API/Services/RegistroClienteIntegrationHandler.cs
@@ -7,31 +7,29 @@ using Microsoft.Extensions.Hosting;
 using NSE.Costumer.API.Application.Commands;
 using NSE.Core.Mediator;
 using NSE.Core.Messages.Integration;
-using EasyNetQ;
+using NSE.MessageBus;
 
 namespace NSE.Costumer.API.Services
 {
     public class RegistroClienteIntegrationHandler : BackgroundService
     {
-        private IBus _bus;
+        private readonly IMessageBus _bus;
         private readonly IServiceProvider _serviceProvider;
 
-        public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider)
+        public RegistroClienteIntegrationHandler(IMessageBus bus, IServiceProvider serviceProvider)
         {
+            _bus = bus;
             _serviceProvider = serviceProvider;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _bus = RabbitHutch.CreateBus("host=localhost:5672");
-
-            _bus.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(async request => 
-                new ResponseMessage(await RegistrarCliente(request)));
+            _bus.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(async request => await RegistrarCliente(request));
 
             return Task.CompletedTask;
         }
 
-        private async Task<ValidationResult> RegistrarCliente(UsuarioRegistradoIntegrationEvent message) 
+        private async Task<ResponseMessage> RegistrarCliente(UsuarioRegistradoIntegrationEvent message) 
         {
             var clienteCommand = new RegistrarClienteCommand(message.Id, message.Nome, message.Email, message.Cpf);
             ValidationResult sucesso;
@@ -42,7 +40,7 @@ namespace NSE.Costumer.API.Services
                 sucesso = await mediator.EnviarComando(clienteCommand);
             }
 
-            return sucesso;
+            return new ResponseMessage(sucesso);
         }
     }
 }
