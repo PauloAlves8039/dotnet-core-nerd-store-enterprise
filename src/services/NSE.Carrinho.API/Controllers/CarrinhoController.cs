8d3a5e24cb1e0d6b982edfcf93f7cd603cbecea4
PauloAlves8039 <pj38alves@gmail.com>
2022-10-26 20:29:54 -0300

    Adicionando o CarrinhoController

---
+++ src/services/NSE.Carrinho.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Carrinho.API.Model;
+using NSE.WebApp.MVC.Controllers;
+using System;
+using System.Threading.Tasks;
+
+namespace NSE.Carrinho.API.Controllers
+{
+    public class CarrinhoController : MainController
+    {
+        [HttpGet("carrinho")]
+        public async Task<CarrinhoCliente> ObterCarrinho()
+        {
+            return null;
+        }
+
+        [HttpPost("carrinho")]
+        public async Task<IActionResult> AdicionarItemCarrinho(CarrinhoItem item) 
+        {
+            return null;
+        }
+
+        [HttpPut("carrinho/{produtoId}")]
+        public async Task<IActionResult> AtualizarItemCarrinho(Guid produtoId, CarrinhoItem item) 
+        {
+            return null;
+        }
+
+        [HttpDelete("carrinho/{produtoId}")]
+        public async Task<IActionResult> RemoverItemCarrinho(Guid produtoId) 
+        {
+            return null;
+        }
+    }
+}
