a8a5ec3d7167f2efb48fde1fef4bac010fe0b878
PauloAlves8039 <pj38alves@gmail.com>
2022-10-27 20:31:50 -0300

    Criando comportamentos de negócio para a API de Carrinho

--- src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NSE.Carrinho.API.Model
 {
@@ -10,6 +11,8 @@ namespace NSE.Carrinho.API.Model
         public decimal ValorTotal { get; set; }
         public List<CarrinhoItem> Itens { get; set; } = new List<CarrinhoItem>();
 
+        internal const int MAX_QUANTIDADE_ITEM = 5;
+
         public CarrinhoCliente(Guid clienteId)
         {
             Id = Guid.NewGuid();
@@ -17,5 +20,40 @@ namespace NSE.Carrinho.API.Model
         }
 
         public CarrinhoCliente() { }
+
+        internal void CalcularValorCarrinho()
+        {
+            ValorTotal = Itens.Sum(p => p.CalcularValor());
+        }
+
+        internal bool CarrinhoItemExistente(CarrinhoItem item)
+        {
+            return Itens.Any(p => p.ProdutoId == item.ProdutoId);
+        }
+
+        internal CarrinhoItem ObterPorProdutoId(Guid produtoId)
+        {
+            return Itens.FirstOrDefault(p => p.ProdutoId == produtoId);
+        }
+
+        internal void AdicionarItem(CarrinhoItem item)
+        {
+            if (!item.EhValido()) return;
+            
+            item.AssociarCarrinho(Id);
+
+            if (CarrinhoItemExistente(item))
+            {
+                var itemExistente = ObterPorProdutoId(item.ProdutoId);
+                itemExistente.AdicionarUnidades(item.Quantidade);
+
+                item = itemExistente;
+                Itens.Remove(itemExistente);
+            }
+
+            Itens.Add(item);
+            CalcularValorCarrinho();
+
+        }
     }
 }
--- src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
+++ src/services/NSE.Carrinho.API/Model/CarrinhoItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 
 namespace NSE.Carrinho.API.Model
 {
@@ -19,5 +20,51 @@ namespace NSE.Carrinho.API.Model
         public Guid CarrinhoId { get; set; }
 
         public CarrinhoCliente CarrinhoCliente { get; set; }
+
+        internal void AssociarCarrinho(Guid carrinhoId)
+        {
+            CarrinhoId = carrinhoId;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * Valor;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal bool EhValido()
+        {
+            return new ItemCarrinhoValidation().Validate(this).IsValid;
+        }
+    }
+
+    public class ItemCarrinhoValidation : AbstractValidator<CarrinhoItem>
+    {
+        public ItemCarrinhoValidation()
+        {
+            RuleFor(c => c.ProdutoId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do produto inválido");
+
+            RuleFor(c => c.Nome)
+                .NotEmpty()
+                .WithMessage("O nome do produto não foi informado");
+
+            RuleFor(c => c.Quantidade)
+                .GreaterThan(0)
+                .WithMessage(item => "A quantidade miníma para o é 1");
+
+            RuleFor(c => c.Quantidade)
+                .LessThan(CarrinhoCliente.MAX_QUANTIDADE_ITEM)
+                .WithMessage(item => $"A quantidade máxima de um item é {CarrinhoCliente.MAX_QUANTIDADE_ITEM}");
+
+            RuleFor(c => c.Valor)
+                .GreaterThan(0)
+                .WithMessage(item => "O valor do precisa ser maior que 0");
+        }
     }
 }
