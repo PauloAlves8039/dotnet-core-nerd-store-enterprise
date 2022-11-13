21db7444f28ea85a030ae242b3d0d7ac9016d34d
PauloAlves8039 <pj38alves@gmail.com>
2022-11-13 15:22:53 -0300

    Implementando a classe AppServicesSettings

--- src/api gateways/NSE.Bff.Compras/Configuration/ApiConfig.cs
+++ src/api gateways/NSE.Bff.Compras/Configuration/ApiConfig.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSE.Bff.Compras.Extensions;
 using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Bff.Compras.Configuration
