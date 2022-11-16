4782f590bab259f71d85c22aa4d8a8267b8c4c8c
PauloAlves8039 <pj38alves@gmail.com>
2022-11-16 20:28:31 -0300

    Implementando a interface ICarrinhoService

--- src/api gateways/NSE.Bff.Compras/Services/CarrinhoService.cs
+++ src/api gateways/NSE.Bff.Compras/Services/CarrinhoService.cs
@@ -17,7 +17,65 @@ namespace NSE.Bff.Compras.Services
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CarrinhoUrl);
         }
+
+        public async Task<CarrinhoDTO> ObterCarrinho()
+        {
+            var response = await _httpClient.GetAsync("/carrinho/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<CarrinhoDTO>(response);
+        }
+
+        public async Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoDTO produto)
+        {
+            var itemContent = ObterConteudo(produto);
+
+            var response = await _httpClient.PostAsync("/carrinho/", itemContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
+        public async Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoDTO carrinho)
+        {
+            var itemContent = ObterConteudo(carrinho);
+
+            var response = await _httpClient.PutAsync($"/carrinho/{carrinho.ProdutoId}", itemContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
+        public async Task<ResponseResult> RemoverItemCarrinho(Guid produtoId)
+        {
+            var response = await _httpClient.DeleteAsync($"/carrinho/{produtoId}");
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
+        public async Task<ResponseResult> AplicarVoucherCarrinho(VoucherDTO voucher)
+        {
+            var itemContent = ObterConteudo(voucher);
+
+            var response = await _httpClient.PostAsync("/carrinho/aplicar-voucher/", itemContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
     }
 
-    public interface ICarrinhoService { }
+    public interface ICarrinhoService 
+    {
+        Task<CarrinhoDTO> ObterCarrinho();
+        Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoDTO produto);
+        Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoDTO carrinho);
+        Task<ResponseResult> RemoverItemCarrinho(Guid produtoId);
+        Task<ResponseResult> AplicarVoucherCarrinho(VoucherDTO voucher);
+    }
 }
