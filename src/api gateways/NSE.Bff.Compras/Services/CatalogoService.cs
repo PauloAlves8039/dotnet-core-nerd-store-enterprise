74f3daa028cf63c922590b8cbdded970a29fd38b
PauloAlves8039 <pj38alves@gmail.com>
2022-11-17 14:19:57 -0300

    Implementando lógica para o CatalogoService

--- src/api gateways/NSE.Bff.Compras/Services/CatalogoService.cs
+++ src/api gateways/NSE.Bff.Compras/Services/CatalogoService.cs
@@ -17,7 +17,31 @@ namespace NSE.Bff.Compras.Services
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
         }
+
+        public async Task<ItemProdutoDTO> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ItemProdutoDTO>(response);
+        }
+
+        public async Task<IEnumerable<ItemProdutoDTO>> ObterItens(IEnumerable<Guid> ids)
+        {
+            var idsRequest = string.Join(",", ids);
+
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/lista/{idsRequest}/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<ItemProdutoDTO>>(response);
+        }
     }
 
-    public interface ICatalogoService { }
+    public interface ICatalogoService 
+    {
+        Task<ItemProdutoDTO> ObterPorId(Guid id);
+        Task<IEnumerable<ItemProdutoDTO>> ObterItens(IEnumerable<Guid> ids);
+    }
 }
