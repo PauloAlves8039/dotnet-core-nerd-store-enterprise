fbfb8245ea77f34f36d2cbf0df5bc51616cf9302
PauloAlves8039 <pj38alves@gmail.com>
2022-12-03 17:27:32 -0300

    Adicionando referências para injeção de dependência

--- src/services/NSE.Pedidos.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Pedidos.API/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using NSE.Core.Mediator;
+using NSE.Pedidos.API.Application.Commands;
+using NSE.Pedidos.API.Application.Events;
+using NSE.Pedidos.API.Application.Queries;
 using NSE.WebAPI.Core.Usuario;
 
 namespace NSE.Pedidos.API.Configuration
@@ -10,6 +16,14 @@ namespace NSE.Pedidos.API.Configuration
         {
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
+
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
+
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
         }
     }
 }
